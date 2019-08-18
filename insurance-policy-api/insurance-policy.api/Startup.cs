f5af6bd100f10cbd023c85cf581b965aaca1d3a7
Daniel Velez <daniel.velez@satrack.com>
2019-08-18 09:12:46 -0500

    test in api

--- insurance-policy-api/insurance-policy.api/Startup.cs
+++ insurance-policy-api/insurance-policy.api/Startup.cs
@@ -38,6 +38,7 @@ namespace insurance_policy.api
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
                 app.UseHsts();
             }
 
